c26de8e005c661ac12a0b12dbe4a1ef7fa5576cb
David Weisdorf <weisdorf.david@gmail.com>
2017-02-23 11:46:40 -0500

    moving user convenience #defines to comm from flexsea_board.h, for PLAN

--- inc/flexsea_comm.h
+++ inc/flexsea_comm.h
@@ -217,7 +217,25 @@ extern MsgQueue unpacked_packet_queue;
 #define update_rx_buf_array_wireless	update_rx_buf_array_3
 
 #endif
-	
+
+#ifdef BOARD_TYPE_FLEXSEA_PLAN
+
+//Overload buffer & function names (for user convenience):
+
+#define comm_str_usb				comm_str_1
+#define unpack_payload_usb			unpack_payload_1
+#define rx_command_usb				rx_command_1
+#define update_rx_buf_byte_usb		update_rx_buf_byte_1
+#define update_rx_buf_array_usb		update_rx_buf_array_1
+
+#define comm_str_spi				comm_str_2
+#define unpack_payload_spi			unpack_payload_2
+#define rx_command_spi				rx_command_2
+#define update_rx_buf_byte_spi		update_rx_buf_byte_2
+#define update_rx_buf_array_spi		update_rx_buf_array_2
+
+#endif
+
 #ifdef __cplusplus
 }
 #endif
