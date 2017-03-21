3b84070638f4ea1c044ce6393e9a4cc51320d3c9
Jean-Francois Duval <jfduval@aqra.ca>
2017-03-21 13:38:56 -0400

    Unit tests for buffers, highlighting bugs.

--- test/test-flexsea_buffers.c
+++ test/test-flexsea_buffers.c
@@ -3,25 +3,117 @@ extern "C" {
 #endif
 #include <flexsea_buffers.h>
 #include "flexsea-comm_test-all.h"
+#include <stdio.h>
 
 //Definitions and variables used by some/all tests:
 //...
 
+#define TEST_LEN	26
 void test_update_rx_buf_byte_1(void)
 {
-	//ToDo
+	int i;
+	uint8_t inputStr[TEST_LEN];
+	for(i = 0; i < TEST_LEN; i++)
+	{
+		inputStr[i] = 'a'+i;
+	}
+	//printf("Test val: %s.\n", inputStr);
+	//fflush(stdout);
+
+	//Start with all 0
+	memset(rx_buf_1, 0, RX_BUF_LEN);
+
+	//Fill buffer:
+	for(i = 0; i < TEST_LEN; i++)
+	{
+		update_rx_buf_byte_1(inputStr[i]);
+	}
+
+	TEST_ASSERT_EQUAL_STRING_LEN(inputStr, rx_buf_1, TEST_LEN);
+	//printf("rx_buf_1: %s.\n", rx_buf_1);
+	//fflush(stdout);
+
+	//Now we keep filling it:
+
+	//Fill buffer:
+	for(i = 0; i < TEST_LEN; i++)
+	{
+		update_rx_buf_byte_1(inputStr[i]);
+	}
+	for(i = 0; i < TEST_LEN; i++)
+	{
+		update_rx_buf_byte_1(inputStr[i]);
+	}
+	for(i = 0; i < TEST_LEN; i++)
+	{
+		update_rx_buf_byte_1(inputStr[i]);
+	}
+
+	//printf("rx_buf_1: %s.\n", rx_buf_1);
+	//fflush(stdout);
+
+	//Final test:
+	for(i = 0; i < RX_BUF_LEN - 1; i++)
+	{
+		if(rx_buf_1[i] != 'z')
+			TEST_ASSERT_EQUAL_UINT8(rx_buf_1[i], rx_buf_1[i+1]-1);
+		else
+			TEST_ASSERT_EQUAL_UINT8('a', rx_buf_1[i+1]);
+	}
 }
 
 void test_update_rx_buf_array_1(void)
 {
-	//ToDo
+	int i;
+	uint8_t inputStr[TEST_LEN];
+	for(i = 0; i < TEST_LEN; i++)
+	{
+		inputStr[i] = 'a'+i;
+	}
+	//printf("Test val: %s.\n", inputStr);
+	//fflush(stdout);
+
+	//Start with all 0
+	memset(rx_buf_1, 0, RX_BUF_LEN);
+
+	//Fill buffer:
+	update_rx_buf_array_1(inputStr, TEST_LEN);
+	printf("rx_buf_1: %s.\n", rx_buf_1);
+	fflush(stdout);
+
+	TEST_ASSERT_EQUAL_STRING_LEN_MESSAGE(inputStr, rx_buf_1, TEST_LEN, "First update");
+
+
+	//Now we keep filling it:
+
+	//Fill buffer:
+	update_rx_buf_array_1(inputStr, TEST_LEN);
+	printf("rx_buf_1: %s.\n", rx_buf_1);
+	fflush(stdout);
+
+	update_rx_buf_array_1(inputStr, TEST_LEN);
+	printf("rx_buf_1: %s.\n", rx_buf_1);
+	fflush(stdout);
+
+	update_rx_buf_array_1(inputStr, TEST_LEN);
+	printf("rx_buf_1: %s.\n", rx_buf_1);
+	fflush(stdout);
+
+	//Final test:
+	for(i = 0; i < RX_BUF_LEN - 1; i++)
+	{
+		if(rx_buf_1[i] != 'z')
+			TEST_ASSERT_EQUAL_UINT8(rx_buf_1[i], rx_buf_1[i+1]-1);
+		else
+			TEST_ASSERT_EQUAL_UINT8('a', rx_buf_1[i+1]);
+	}
 }
 
 void test_buffer_stack(void)
 {
 	int i;
 
-	//Start will all 0
+	//Start with all 0
 	memset(rx_buf_1, 0, RX_BUF_LEN);
 
 	for(i = 0; i < 100; i++)
@@ -62,6 +154,8 @@ void test_buffer_circular(void)
 void test_flexsea_buffers(void)
 {
 	UNITY_BEGIN();
+	//RUN_TEST(test_update_rx_buf_byte_1);
+	RUN_TEST(test_update_rx_buf_array_1);
 	RUN_TEST(test_buffer_stack);
 	RUN_TEST(test_buffer_circular);
 	UNITY_END();
