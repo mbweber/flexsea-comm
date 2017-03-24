5c1d60a5b3a001fff832264cc51d08b51ae4bb1b
Jean-Francois Duval <jfduval@aqra.ca>
2017-03-24 14:57:59 -0400

    Added circular buffers 2 & 3.

--- inc/flexsea_buffers.h
+++ inc/flexsea_buffers.h
@@ -86,7 +86,7 @@ void test_upd(void);
 // New code - not integrated - test in progress:
 //****************************************************************************
 
-extern circularBuffer_t rx_buf_circ_1;
+extern circularBuffer_t rx_buf_circ_1, rx_buf_circ_2, rx_buf_circ_3;
 
 #ifdef __cplusplus
 }
