285f219dcc7fb9e0c2f31007cecd31288251bc14
Jean-Francois Duval <jfduval@aqra.ca>
2017-10-11 13:31:17 -0400

    Changed array size to avoid the memory leak we detected on the Pi

--- src/flexsea_comm.c
+++ src/flexsea_comm.c
@@ -70,24 +70,24 @@ extern "C" {
 
 uint8_t comm_str_tmp[COMM_STR_BUF_LEN];
 
-uint8_t comm_str_1[COMM_STR_BUF_LEN];
-uint8_t packed_1[COMM_STR_BUF_LEN];
-uint8_t rx_command_1[PACKAGED_PAYLOAD_LEN];
-uint8_t comm_str_2[COMM_STR_BUF_LEN];
-uint8_t packed_2[COMM_STR_BUF_LEN];
-uint8_t rx_command_2[PACKAGED_PAYLOAD_LEN];
-uint8_t comm_str_3[COMM_STR_BUF_LEN];
-uint8_t packed_3[COMM_STR_BUF_LEN];
-uint8_t rx_command_3[PACKAGED_PAYLOAD_LEN];
-uint8_t comm_str_4[COMM_STR_BUF_LEN];
-uint8_t packed_4[COMM_STR_BUF_LEN];
-uint8_t rx_command_4[PACKAGED_PAYLOAD_LEN];
-uint8_t comm_str_5[COMM_STR_BUF_LEN];
-uint8_t packed_5[COMM_STR_BUF_LEN];
-uint8_t rx_command_5[PACKAGED_PAYLOAD_LEN];
-uint8_t comm_str_6[COMM_STR_BUF_LEN];
-uint8_t packed_6[COMM_STR_BUF_LEN];
-uint8_t rx_command_6[PACKAGED_PAYLOAD_LEN];
+uint8_t comm_str_1[COMM_PERIPH_ARR_LEN];
+uint8_t packed_1[COMM_PERIPH_ARR_LEN];
+uint8_t rx_command_1[COMM_PERIPH_ARR_LEN];
+uint8_t comm_str_2[COMM_PERIPH_ARR_LEN];
+uint8_t packed_2[COMM_PERIPH_ARR_LEN];
+uint8_t rx_command_2[COMM_PERIPH_ARR_LEN];
+uint8_t comm_str_3[COMM_PERIPH_ARR_LEN];
+uint8_t packed_3[COMM_PERIPH_ARR_LEN];
+uint8_t rx_command_3[COMM_PERIPH_ARR_LEN];
+uint8_t comm_str_4[COMM_PERIPH_ARR_LEN];
+uint8_t packed_4[COMM_PERIPH_ARR_LEN];
+uint8_t rx_command_4[COMM_PERIPH_ARR_LEN];
+uint8_t comm_str_5[COMM_PERIPH_ARR_LEN];
+uint8_t packed_5[COMM_PERIPH_ARR_LEN];
+uint8_t rx_command_5[COMM_PERIPH_ARR_LEN];
+uint8_t comm_str_6[COMM_PERIPH_ARR_LEN];
+uint8_t packed_6[COMM_PERIPH_ARR_LEN];
+uint8_t rx_command_6[COMM_PERIPH_ARR_LEN];
 
 uint32_t cmd_valid = 0;
 uint32_t cmd_bad_checksum = 0;
